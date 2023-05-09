aef461243fde4ec27f2e99879d7372e5fa4508cd
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2023-05-09 18:44:36 +0900

    update layout

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,9 +1,5 @@
-import { Slot, component$, useStyles$ } from '@builder.io/qwik';
+import { Slot, component$ } from '@builder.io/qwik';
 import { routeLoader$ } from '@builder.io/qwik-city';
-import Footer from '~/components/starter/footer/footer';
-import Header from '~/components/starter/header/header';
-
-import styles from './styles.css?inline';
 
 export const useServerTimeLoader = routeLoader$(() => {
   return {
@@ -12,14 +8,11 @@ export const useServerTimeLoader = routeLoader$(() => {
 });
 
 export default component$(() => {
-  useStyles$(styles);
   return (
     <>
-      <Header />
       <main>
         <Slot />
       </main>
-      <Footer />
     </>
   );
 });
